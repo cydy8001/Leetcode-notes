51527eef16f5f0c1954661953cff9dae56ff62ae
cydy8001 <55064351+cydy8001@users.noreply.github.com>
2020-03-02 22:52:33 -0500

    Update 46. Permutation.py

--- Leetcode Python/Array/46. Permutation.py
+++ Leetcode Python/Array/46. Permutation.py
@@ -1,9 +1,9 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         if len(nums) <= 1:
-            return [nums]
-        answer = []
-        for i, num in enumerate(nums):
+            return [nums] #terminate recursion
+        answer = [] #declare answer
+        for i, num in enumerate(nums): #fix pivots
             n = nums[:i] + nums[i+1:]
             for y in self.permute(n):
                 answer.append([num] + y)
